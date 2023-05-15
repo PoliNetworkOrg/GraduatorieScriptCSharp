a97e738c487dfcacad1873186cae3bc44293022b
github.nouser@nouser.github.com <github.nouser@nouser.github.com>
2023-05-16 00:25:21 +0200

    Update .gitignore and HtmlNodeExtensions.cs

---
+++ Utils/HtmlNodeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using HtmlAgilityPack;
+
+namespace GraduatorieScript;
+
+public static class HtmlNodeExtensions
+{
+    public static IEnumerable<HtmlNode> GetElementsByName(this HtmlNode parent, string name)
+    {
+        return parent.Descendants().Where(node => node.Name == name);
+    }
+
+    public static IEnumerable<HtmlNode> GetElementsByTagName(this HtmlNode parent, string name)
+    {
+        return parent.Descendants(name);
+    }
+}
