e900f2d8c277f26e2c07f5633d4198d81581ab14
github.nouser@nouser.github.com <github.nouser@nouser.github.com>
2023-05-18 23:20:06 +0200

    Update Ranking.cs and StudentResult.cs

--- Objects/Ranking.cs
+++ Objects/Ranking.cs
@@ -1,6 +1,17 @@
-﻿namespace GraduatorieScript.Objects;
+﻿using Newtonsoft.Json;
 
+namespace GraduatorieScript.Objects;
+
+[Serializable]
+[JsonObject(MemberSerialization.Fields)]
 public class Ranking
 {
-    
+    private string year;
+    private string school;
+    private string phase;
+    private string extra;
+    private List<StudentResult> byMerit;
+    private List<StudentResult> byId;
+    private Dictionary<string, List<StudentResult>> byDegree;
+    private string url;
 }
--- Objects/StudentResult.cs
+++ Objects/StudentResult.cs
@@ -1,6 +1,19 @@
-﻿namespace GraduatorieScript.Objects;
+﻿using Newtonsoft.Json;
 
+namespace GraduatorieScript.Objects;
+
+[Serializable]
+[JsonObject(MemberSerialization.Fields)]
 public class StudentResult
 {
-    
+    private string id;
+    private DateOnly birthDate;
+    private int positionMeritInDegree;
+    private int positionMeritAbsolute;
+    private int englishCorrectAnswers;
+    private decimal result;
+    private Dictionary<string, decimal> partialResults;
+    private bool canEnroll;
+    private string canEnrollInto;
+    private Dictionary<string, bool> ofa;
 }
