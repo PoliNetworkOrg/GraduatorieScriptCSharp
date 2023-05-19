bd8e61d3b5cabc98b777eb7a996302c073eca233
github.nouser@nouser.github.com <github.nouser@nouser.github.com>
2023-05-19 14:33:49 +0200

    Update LinksFind.cs

--- Utils/Web/LinksFind.cs
+++ Utils/Web/LinksFind.cs
@@ -4,14 +4,26 @@ public static class LinksFind
 {
     public static HashSet<string?> FindLinksFromWeb()
     {
+        var findRankingsLink = FindLinksFromPolimiNews();
+        var linksFromCombinations = FindLinksFromCombinations();
+
+        //merge results links
+        var listOfListOfLinks = new List<List<string>> { findRankingsLink , linksFromCombinations};
+        var rankingsLinks = Strings.StringUtil.Merge(listOfListOfLinks);
+        return rankingsLinks;
+    }
+
+    private static List<string> FindLinksFromCombinations()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static List<string> FindLinksFromPolimiNews()
+    {
         //scrape links from polimi news
         var scraper = new Scraper();
         var links = scraper.GetNewsLinks();
         var findRankingsLink = scraper.FindRankingsLink(links);
-
-        //merge results links
-        var listOfListOfLinks = new List<List<string>> { findRankingsLink };
-        var rankingsLinks = GraduatorieScript.Utils.Strings.StringUtil.Merge(listOfListOfLinks);
-        return rankingsLinks;
+        return findRankingsLink;
     }
 }
