5763f5751e5b83d9d9dd082e0ca721593c3507cd
github.nouser@nouser.github.com <github.nouser@nouser.github.com>
2023-05-26 12:47:51 +0200

    Update Program.cs

--- GraduatorieScript/Main/Program.cs
+++ GraduatorieScript/Main/Program.cs
@@ -10,8 +10,6 @@ namespace GraduatorieScript.Main;
 
 public static class Program
 {
-// See https://aka.ms/new-console-template for more information
-
     public static void Main()
     {
         var mt = new Metrics();
@@ -19,36 +17,36 @@ public static class Program
         var baseFolder = PathUtils.FindDocsFolder();
         Console.WriteLine($"[INFO] baseFolder: {baseFolder}");
 
-//find links from web
+        //find links from web
         var rankingsUrls = mt.Execute(LinksFind.GetAll);
 
-//print links found
+        //print links found
         foreach (var r in rankingsUrls) Console.WriteLine($"[DEBUG] valid url found: {r.url}");
 
-// todo: handle when baseFolder is null 
+        // todo: handle when baseFolder is null 
         var outputJsonPath = Path.Join(baseFolder, Constants.OutputJsonFilename);
 
-//nella cartella trovata, leggere e analizzare gli eventuali file .html
+        //nella cartella trovata, leggere e analizzare gli eventuali file .html
         var rankingsSetFromHtmls = Parser.FindParseHtmls(baseFolder);
 
-//estraiamo i risultati dal web
+        //estraiamo i risultati dal web
         var rankingsSetFromWeb = Parser.ParseWeb(rankingsUrls);
 
-//estraiamo i risultati da un eventuale json locale
+        //estraiamo i risultati da un eventuale json locale
         var rankingsSetFromLocalJson = Parser.ParseLocalJson(outputJsonPath);
 
-//uniamo i dataset (quello dall'html, quello dal json locale, quello dal web)
+        //uniamo i dataset (quello dall'html, quello dal json locale, quello dal web)
         var fullRankingsSet = RankingsSet.Merge(rankingsSetFromHtmls, rankingsSetFromWeb, rankingsSetFromLocalJson);
 
-//ottenere un json 
+        //ottenere un json 
         var stringJson = JsonConvert.SerializeObject(fullRankingsSet);
 
-//scriviamolo su disco
+        //scriviamolo su disco
         File.WriteAllText(outputJsonPath, stringJson);
 
-//eliminare i suddetti file html
-/* if (transformerResult?.pathFound != null) */
-/*     FileUtils.TryBulkDelete(transformerResult.pathFound); */
-// ^^ this must be wrong
+        //eliminare i suddetti file html
+        /* if (transformerResult?.pathFound != null) */
+        /*     FileUtils.TryBulkDelete(transformerResult.pathFound); */
+        // ^^ this must be wrong
     }
 }
