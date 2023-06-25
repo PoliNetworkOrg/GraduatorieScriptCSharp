3f4eb91cc185b7e8d1634fca5059ea01f422b433
angeousta <angeousta@outlook.it>
2023-06-25 22:46:27 +0200

    Update GraduatorieScript.csproj, RankingSummary.cs, Parser.cs, and 1 more file

---
+++ GraduatorieScript/Utils/Transformer/StatsCalculate.cs
@@ -0,0 +1,41 @@
+﻿using GraduatorieScript.Objects;
+
+namespace GraduatorieScript.Utils.Transformer;
+
+public static class StatsCalculate
+{
+    public static void CalculateStats(Ranking ranking)
+    {
+        ranking.RankingSummary = CalculateStatsSingle(ranking);
+    }
+
+    private static RankingSummary CalculateStatsSingle(Ranking ranking)
+    {
+        var byMeritRows = ranking.byMerit?.Rows;
+        var results = CalculateResultsScores(byMeritRows);
+        
+        var rankingSummary = new RankingSummary()
+            {
+                HowManyCanEnroll = byMeritRows?.Count(x => x.canEnroll),
+                HowManyStudents = byMeritRows?.Count,
+                ResultsSummarized = results
+            };
+
+        return rankingSummary;
+    }
+
+    private static Dictionary<int, int>? CalculateResultsScores(IReadOnlyCollection<StudentResult>? byMeritRows)
+    {
+        if (byMeritRows == null) return null;
+        
+        var results = new Dictionary<int, int>();
+        var enumerable = byMeritRows.Select(variable => (int)Math.Round(variable.result));
+        foreach (var score in enumerable)
+        {
+            results.TryAdd(score, 0);
+            results[score] += 1;
+        }
+
+        return results;
+    }
+}
