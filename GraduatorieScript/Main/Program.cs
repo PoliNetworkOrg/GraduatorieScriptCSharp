5903e8f36eee70d32f19d9c805e7eea06ec89032
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2023-06-26 22:42:32 +0200

    chore(Program): little refactor

--- GraduatorieScript/Main/Program.cs
+++ GraduatorieScript/Main/Program.cs
@@ -14,46 +14,43 @@ public static class Program
         var mt = new Metrics();
 
         var docsFolder = GetDocsFolder(args);
-        Console.WriteLine($"[INFO] baseFolder: {docsFolder}");
-
-        if (string.IsNullOrEmpty(docsFolder))
-        {
-            Console.WriteLine("[INFO] baseFolder is null. Abort.");
-            return;
-        }
+        Console.WriteLine($"[INFO] docsFolder: {docsFolder}");
 
         //find links from web
-        var rankingsUrls = mt.Execute(LinksFind.GetAll).ToList();
+        var rankingsUrls = mt.Execute(LinksFind.GetAll);
         ScraperOutput.Write(rankingsUrls, docsFolder);
 
         //print links found
         foreach (var r in rankingsUrls)
             Console.WriteLine($"[DEBUG] valid url found: {r.Url}");
 
-        // ricava un unico set partendo dai file html salvati, dagli url trovati e
-        // dal precedente set salvato nel .json
-        var outFolder = Path.Join(docsFolder, Constants.OutputFolder);
+        // ricava un unico set partendo dai file html salvati, dagli url 
+        // trovati e dal precedente set salvato nel .json
         var rankingsSet = Parser.GetRankings(docsFolder, rankingsUrls);
+
+        // salvare il set
+        var outFolder = Path.Join(docsFolder, Constants.OutputFolder);
         MainJson.Write(outFolder, rankingsSet);
         StatsJson.Write(outFolder, rankingsSet);
-
-
-        //eliminare i suddetti file html
-        /* if (transformerResult?.pathFound != null) */
-        /*     FileUtils.TryBulkDelete(transformerResult.pathFound); */
-        // ^^ this must be wrong
     }
 
     private static string GetDocsFolder(IReadOnlyList<string> args)
     {
-        var folder = args.Count > 0 ? args[0] : null;
-        var b = args.Count > 0 && !string.IsNullOrEmpty(folder);
-        var docsFolder = b
-            ? folder
+        // check if docsFolder passed in args
+        var argsFolder = args.Count > 0 ? args[0] : null;
+
+        // use it if passed or search the default
+        var docsFolder = !string.IsNullOrEmpty(argsFolder)
+            ? argsFolder
             : PathUtils.FindFolder(Constants.FolderToFind);
 
-        return !string.IsNullOrEmpty(docsFolder)
-            ? docsFolder
-            : PathUtils.CreateAndReturnDocsFolder(Constants.FolderToFind);
+        // if not found, create it
+        if (string.IsNullOrEmpty(docsFolder))
+        {
+            Console.WriteLine("[WARNING] docsFolder not found, creating it");
+            return PathUtils.CreateAndReturnDocsFolder(Constants.FolderToFind);
+        }
+
+        return docsFolder;
     }
 }
