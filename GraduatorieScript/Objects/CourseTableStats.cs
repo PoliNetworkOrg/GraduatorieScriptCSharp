8bd08433e40a91405191185cfdd1cb3bba64e057
angeousta <angeousta@outlook.it>
2023-06-27 00:31:08 +0200

    Update CourseTableStats.cs

--- GraduatorieScript/Objects/CourseTableStats.cs
+++ GraduatorieScript/Objects/CourseTableStats.cs
@@ -8,8 +8,11 @@ public class CourseTableStats
 {
     public string? Location;
     public string? Title;
-    public decimal? Average;
+    public decimal? AverageScoresOfAllStudents;
     public decimal? AverageOfWhoPassed;
+    public double? AverageBirthYear;
+    public double? AverageEnglishCorrectAnswers;
+    public Dictionary<string, decimal?>? AveragePartialScores;
 
     public static CourseTableStats From(CourseTable courseTable)
     {
@@ -18,8 +21,47 @@ public class CourseTableStats
         {
             Location = courseTable.Location,
             Title = courseTable.Title,
-            Average = courseTableRows?.Select(x => x.result).Average(),
-            AverageOfWhoPassed = courseTableRows?.Where(x => x.canEnroll).Select(x => x.result).Average()
+            AverageScoresOfAllStudents = courseTableRows?.Select(x => x.result).Average(),
+            AverageOfWhoPassed = courseTableRows?.Where(x => x.canEnroll).Select(x => x.result).Average(),
+            AverageBirthYear = courseTableRows?.Select(x => x.birthDate?.Year).Average(),
+            AverageEnglishCorrectAnswers = courseTableRows?.Select(x => x.englishCorrectAnswers).Average(),
+            AveragePartialScores = AveragePartialScoresCalculate(courseTableRows)
         };
+    }
+
+    private static Dictionary<string, decimal?> AveragePartialScoresCalculate(List<StudentResult>? courseTableRows)
+    {
+        var scores = new Dictionary<string, decimal?>();
+
+        var keys = courseTableRows?.Select(x => x.sectionsResults?.Keys).ToList();
+        var keysDistinct = Distinct(keys);
+
+        foreach (var key in keysDistinct)
+        {
+            scores[key] = AveragePartialScoresOfASingleKey(courseTableRows, key);
+        }
+
+        return scores;
+    }
+
+    private static decimal? AveragePartialScoresOfASingleKey(List<StudentResult>? courseTableRows, string key)
+    {
+        return courseTableRows?.Select(x => x.sectionsResults?[key]).Average();
+    }
+
+    private static HashSet<string> Distinct(List<Dictionary<string, decimal>.KeyCollection?>? keys)
+    {
+        var hashSet = new HashSet<string>();
+        if (keys == null) return hashSet;
+        foreach (var v1 in keys)
+        {
+            if (v1 == null) continue;
+            foreach (var v2 in v1)
+            {
+                hashSet.Add(v2);
+            }
+        }
+
+        return hashSet;
     }
 }
