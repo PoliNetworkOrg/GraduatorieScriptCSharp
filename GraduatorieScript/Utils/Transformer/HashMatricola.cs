0d95ca31abf738a8355691ed65d40cec26674473
angeousta <angeousta@outlook.it>
2023-06-28 20:35:46 +0200

    Update HashMatricola.cs

--- GraduatorieScript/Utils/Transformer/HashMatricola.cs
+++ GraduatorieScript/Utils/Transformer/HashMatricola.cs
@@ -1,15 +1,39 @@
-﻿namespace GraduatorieScript.Utils.Transformer;
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace GraduatorieScript.Utils.Transformer;
 
 public static class HashMatricola
 {
+    private const string SaltGlobal = "saltPoliNetwork";
+    private const int MaxCharHash = 20;
+    
     public static string? HashMatricolaMethod(string? input)
     {
-        /*
-            riga_matricola = row.select(".Dati1")[1]
-            sha256_hash = hashlib.sha256((riga_matricola.string + saltGlobal).encode()).hexdigest()
-            sha256_hash = sha256_hash[:maxCharHash]
-            riga_matricola.string = sha256_hash
-         */
-        return input;
+        if (string.IsNullOrEmpty(input))
+            return null;
+
+        var stringInputWithSalt = input + SaltGlobal;
+        var hexHash = GetSha256(stringInputWithSalt);
+        var hashMatricolaMethod = hexHash[..MaxCharHash];
+        return hashMatricolaMethod;
+    }
+
+    private static string GetSha256(string stringInputWithSalt)
+    {
+        var bytes = Encoding.UTF8.GetBytes(stringInputWithSalt);
+        var hashBytes = SHA256.HashData(bytes);
+        var hexHash = ByteArrayToHexString(hashBytes);
+        return hexHash;
+    }
+
+    private static string ByteArrayToHexString(IReadOnlyCollection<byte> bytes)
+    {
+        var sb = new StringBuilder(bytes.Count * 2);
+        foreach (var b in bytes)
+        {
+            sb.Append(b.ToString("X2"));
+        }
+        return sb.ToString();
     }
 }
