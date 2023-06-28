65345bcb9770011483aa727fbf3d352a70ae762e
angeousta <angeousta@outlook.it>
2023-06-28 20:28:52 +0200

    Update HashMatricola.cs

--- GraduatorieScript/Utils/Transformer/HashMatricola.cs
+++ GraduatorieScript/Utils/Transformer/HashMatricola.cs
@@ -4,6 +4,12 @@ public static class HashMatricola
 {
     public static string? HashMatricolaMethod(string? input)
     {
+        /*
+            riga_matricola = row.select(".Dati1")[1]
+            sha256_hash = hashlib.sha256((riga_matricola.string + saltGlobal).encode()).hexdigest()
+            sha256_hash = sha256_hash[:maxCharHash]
+            riga_matricola.string = sha256_hash
+         */
         return input;
     }
 }
