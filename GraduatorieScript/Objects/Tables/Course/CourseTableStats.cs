15b29a05c3184681759cceb52a0fcdc332432932
angeousta <angeousta@outlook.it>
2023-06-29 18:12:52 +0200

    Update CourseTableStats.cs

--- GraduatorieScript/Objects/Tables/Course/CourseTableStats.cs
+++ GraduatorieScript/Objects/Tables/Course/CourseTableStats.cs
@@ -1,7 +1,7 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
-namespace GraduatorieScript.Objects.Tables;
+namespace GraduatorieScript.Objects.Tables.Course;
 
 [Serializable]
 [JsonObject(MemberSerialization.Fields, NamingStrategyType = typeof(CamelCaseNamingStrategy))]
