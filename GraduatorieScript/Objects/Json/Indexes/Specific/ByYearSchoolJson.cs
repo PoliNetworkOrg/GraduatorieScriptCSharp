f36f9aefec1b15bac876c1784ec64ae6280f1162
angeousta <angeousta@outlook.it>
2023-06-29 15:50:24 +0200

    Update ByYearSchoolJson.cs

--- GraduatorieScript/Objects/Json/Indexes/Specific/ByYearSchoolJson.cs
+++ GraduatorieScript/Objects/Json/Indexes/Specific/ByYearSchoolJson.cs
@@ -52,7 +52,7 @@ public class ByYearSchoolJson : IndexJsonBase
         var mainJsonPath = Path.Join(outFolder, PathCustom);
         try
         {
-            var mainJson = Parser.ParseJson<BySchoolYearJson>(mainJsonPath);
+            var mainJson = Parser.ParseJson<ByYearSchoolJson>(mainJsonPath);
             if (mainJson is null)
                 return null;
 
@@ -68,20 +68,20 @@ public class ByYearSchoolJson : IndexJsonBase
         return null;
     }
 
-    private static List<Ranking> RankingsAdd(BySchoolYearJson mainJson, string outFolder)
+    private static List<Ranking> RankingsAdd(ByYearSchoolJson mainJson, string outFolder)
     {
         List<Ranking> rankings = new();
-        foreach (var school in mainJson.Schools)
-        foreach (var year in school.Value)
-        foreach (var filename in year.Value)
-            RankingAdd(school, year, outFolder, filename, rankings);
+        foreach (var year in mainJson.Years)
+        foreach (var school in year.Value)
+        foreach (var filename in school.Value)
+            RankingAdd(year, school, outFolder, filename, rankings);
 
         return rankings;
     }
 
     private static void RankingAdd(
-        KeyValuePair<SchoolEnum, Dictionary<int, IEnumerable<SingleCourseJson>>> school,
-        KeyValuePair<int, IEnumerable<SingleCourseJson>> year,
+        KeyValuePair<int, Dictionary<SchoolEnum, IEnumerable<SingleCourseJson>>> year,
+        KeyValuePair<SchoolEnum, IEnumerable<SingleCourseJson>> school,
         string outFolder,
         SingleCourseJson filename,
         ICollection<Ranking> rankings)
