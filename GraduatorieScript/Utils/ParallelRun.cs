946de0ceafbd86408e6309d6b5f09b689c3d441e
angeousta <angeousta@outlook.it>
2023-06-30 13:38:43 +0200

    debug

--- GraduatorieScript/Utils/ParallelRun.cs
+++ GraduatorieScript/Utils/ParallelRun.cs
@@ -4,13 +4,14 @@ namespace GraduatorieScript.Utils;
 
 public static class ParallelRun
 {
-    private const int Max = 50;
+    private const int MaxWithoutDebug = 50;
+    private static readonly int MaxWithDebug = System.Diagnostics.Debugger.IsAttached ? 1 : MaxWithoutDebug;
     private static readonly int MaxThreadCount = Process.GetCurrentProcess().MaxWorkingSet.ToInt32();
     private static readonly int MaxCoreCount = Environment.ProcessorCount;
 
     public static void Run(Action[] toArray)
     {
-        var maxDegreeOfParallelism = Math.Max(Math.Min(MaxCoreCount, Math.Min(Max, MaxThreadCount)), 1);
+        var maxDegreeOfParallelism = Math.Max(Math.Min(MaxCoreCount, Math.Min(MaxWithDebug, MaxThreadCount)), 1);
         var parallelOptions = new ParallelOptions { MaxDegreeOfParallelism = maxDegreeOfParallelism };
         Parallel.Invoke(parallelOptions, toArray);
     }
