9fe35a23cc3f2e2746e68490721d185706cb4489
angeousta <132761637+angeousta@users.noreply.github.com>
2023-07-11 01:03:17 +0200

    Refactor17 (#100)
    * Update ArgsConfig.cs, Program.cs, and PoliNetwork.Graduatorie.Scraper.csproj
    
    * Update ArgsConfig.cs, RankingSummary.cs, Program.cs, and 1 more file
    
    * Update Program.cs
    
    * Update PoliNetwork.Graduatorie.Scraper.csproj
    
    * Update ArgsConfig.cs, Program.cs, and Program.cs
    
    * Update Program.cs
    
    * Update ArgsConfig.cs, Program.cs, and PoliNetwork.Graduatorie.Scraper.csproj
    
    * Update Program.cs

--- PoliNetwork.Graduatorie.Common/Objects/ArgsConfig.cs
+++ PoliNetwork.Graduatorie.Common/Objects/ArgsConfig.cs
@@ -1,5 +1,8 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using PoliNetwork.Graduatorie.Common.Data;
+using PoliNetwork.Graduatorie.Common.Utils.ParallelNS;
+using PoliNetwork.Graduatorie.Common.Utils.Path;
 
 namespace PoliNetwork.Graduatorie.Common.Objects;
 
@@ -9,4 +12,52 @@ public class ArgsConfig
 {
     public string? DataFolder;
     public bool? ForceReparsing;
+
+    public static ArgsConfig GetArgsConfig(IReadOnlyList<string> args)
+    {
+        var argsConfig = new ArgsConfig
+        {
+            DataFolder = GetDataFolder(FindArgString(args, "--data")),
+            ForceReparsing = FindArgPresent(args, "--reparse")
+        };
+        return argsConfig;
+    }
+
+    private static string GetDataFolder(string? argsFolder)
+    {
+        // use it if passed or search the default
+        var dataFolder = !string.IsNullOrEmpty(argsFolder)
+            ? argsFolder
+            : PathUtils.FindFolder(Constants.DataFolder);
+
+
+        if (!string.IsNullOrEmpty(dataFolder)) return dataFolder;
+
+        // if not found, create it
+        Console.WriteLine("[WARNING] dataFolder not found, creating it");
+        return PathUtils.CreateAndReturnDataFolder(Constants.DataFolder);
+    }
+
+    private static bool? FindArgPresent(IEnumerable<string> args, string reparse)
+    {
+        return args.Any(x => x == reparse);
+    }
+
+    private static string? FindArgString(IReadOnlyList<string> args, string data)
+    {
+        for (var i = 0; i < args.Count; i++)
+        {
+            var s = args[i];
+            if (s != data) continue;
+            if (i + 1 < args.Count) return args[i + 1];
+        }
+
+        return null;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"[INFO] dataFolder: {DataFolder}");
+        Console.WriteLine($"[INFO] thread max count: {ParallelRun.MaxDegreeOfParallelism}");
+    }
 }
---
+++ PoliNetwork.Graduatorie.Scraper/Main/Program.cs
@@ -0,0 +1,47 @@
+﻿using PoliNetwork.Core.Utils;
+using PoliNetwork.Graduatorie.Common.Objects;
+using PoliNetwork.Graduatorie.Common.Objects.RankingNS;
+using PoliNetwork.Graduatorie.Scraper.Utils.Web;
+
+namespace PoliNetwork.Graduatorie.Scraper.Main;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var mt = new Metrics();
+
+        var argsConfig = ArgsConfig.GetArgsConfig(args);
+        argsConfig.Print();
+
+
+        //find links from web
+        var rankingsUrls = RankingsUrls(mt, argsConfig);
+
+        //print and write results
+        PrintAndWriteResults(rankingsUrls, argsConfig);
+    }
+
+    private static void PrintAndWriteResults(List<RankingUrl> rankingsUrls, ArgsConfig argsConfig)
+    {
+        //write results to file
+        ScraperOutput.Write(rankingsUrls, argsConfig.DataFolder);
+
+        //print links found
+        PrintLinks(rankingsUrls);
+    }
+
+    public static List<RankingUrl> RankingsUrls(Metrics mt, ArgsConfig argsConfig)
+    {
+        var rankingsUrls = mt.Execute(LinksFind.GetAll).ToList();
+        rankingsUrls = ScraperOutput.GetWithUrlsFromLocalFileLinks(rankingsUrls, argsConfig.DataFolder);
+        return rankingsUrls;
+    }
+
+
+    private static void PrintLinks(List<RankingUrl> rankingsUrls)
+    {
+        foreach (var r in rankingsUrls)
+            Console.WriteLine($"[DEBUG] valid url found: {r.Url}");
+    }
+}
