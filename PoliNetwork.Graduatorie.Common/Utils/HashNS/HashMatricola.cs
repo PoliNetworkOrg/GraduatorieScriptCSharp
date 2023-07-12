9c292f21d68c22e5683627949c4b50530553b6d9
angeousta <angeousta@outlook.it>
2023-07-12 19:33:49 +0200

    Update HashMatricola.cs

--- PoliNetwork.Graduatorie.Common/Utils/HashNS/HashMatricola.cs
+++ PoliNetwork.Graduatorie.Common/Utils/HashNS/HashMatricola.cs
@@ -1,5 +1,6 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace PoliNetwork.Graduatorie.Common.Utils.HashNS;
 
@@ -8,10 +9,20 @@ public static class HashMatricola
     private const string SaltGlobal = "saltPoliNetwork";
     private const int MaxCharHash = 20;
 
+    private static string RemoveNonAlphanumeric(string input)
+    {
+        // Remove non-alphanumeric characters using regular expressions
+        return Regex.Replace(input, "[^a-zA-Z0-9]", "");
+    }
+    
     public static string? HashMatricolaMethod(string? input)
     {
         if (string.IsNullOrEmpty(input))
             return null;
+
+        input = input.Trim();
+
+        input = RemoveNonAlphanumeric(input);
 
         var stringInputWithSalt = input + SaltGlobal;
         var hexHash = GetSha256(stringInputWithSalt);
