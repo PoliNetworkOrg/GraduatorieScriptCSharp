3e01d7df7f2181f564c496e653bca4402d5ea185
angeousta <angeousta@outlook.it>
2023-07-14 16:28:28 +0200

    Update HashMatricoleWrite.cs

---
+++ PoliNetwork.Graduatorie.Parser/Utils/Output/HashMatricoleWrite.cs
@@ -0,0 +1,11 @@
+using PoliNetwork.Graduatorie.Parser.Objects.RankingNS;
+
+namespace PoliNetwork.Graduatorie.Parser.Utils.Output;
+
+public static class HashMatricoleWrite
+{
+    public static void Write(RankingsSet rankingsSet, string outFolder)
+    {
+        throw new NotImplementedException();
+    }
+}
