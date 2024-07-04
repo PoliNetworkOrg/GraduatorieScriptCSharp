c1ad82df9946ed84f7b40b0e4ca452bf345738df
Lorenzo Corallo <66379281+lorenzocorallo@users.noreply.github.com>
2024-07-04 19:16:22 +0200

    fix: correct extra-eu validation (#159)

--- PoliNetwork.Graduatorie.Parser/Objects/RankingNS/RankingOrder.cs
+++ PoliNetwork.Graduatorie.Parser/Objects/RankingNS/RankingOrder.cs
@@ -11,53 +11,49 @@ namespace PoliNetwork.Graduatorie.Parser.Objects.RankingNS;
 [JsonObject(MemberSerialization.Fields, NamingStrategyType = typeof(CamelCaseNamingStrategy))]
 public class RankingOrder
 {
-    public bool? Anticipata;
-    public bool? ExtraEu;
-
-    public string? Phase;
+    public string? Phase; // the original string (e.g. "
 
     //esempio:
     //seconda graduatoria di seconda fase: {primary:2,secondary:2}
     //prima graduatoria di seconda fase:{primary:2, secondary:1}
     public int? Primary;
     public int? Secondary;
-    public bool IsEnglish = false;
+    public bool IsAnticipata; // used for DES/URB rankings until 2023
+    public bool IsExtraEu;
+    public bool IsEnglish;
 
-    public RankingOrder()
-    {
-    }
-
-    public RankingOrder(string phase, bool isEnglish = false)
+    public RankingOrder(string phase, bool isExtraEu = false, bool isEnglish = false)
     {
         Phase = phase;
+        ParsePhaseString(phase);
+        
+        IsExtraEu = isExtraEu;
         IsEnglish = isEnglish;
-        FixValues();
     }
 
-    private void FixValues()
+    private void ParsePhaseString(string phase)
     {
-        var s = Phase?.ToUpper().Trim() ?? "";
-        if (string.IsNullOrEmpty(s))
-            return;
-
-        ExtraEu = s.Contains("EXTRA");
+        var s = phase.ToUpper().Trim();
+        if (string.IsNullOrEmpty(s)) return;
+        
         var strings = s.Split(" ");
-        Primary = GetCount(strings, "FASE");
-        Secondary = GetCount(strings, "GRADUATORIA");
-        Anticipata = s.Contains("ANTICIPATA");
+        
+        IsAnticipata = s.Contains("ANTICIPATA");
+        if (IsAnticipata) return;
+        
+        Primary = ExtractPhaseNumberByKey(strings, "FASE");
+        Secondary = ExtractPhaseNumberByKey(strings, "GRADUATORIA");
     }
 
-    private static int? GetCount(IReadOnlyList<string> s, string key)
+    private static int? ExtractPhaseNumberByKey(IReadOnlyList<string> s, string key)
     {
-        for (var i = 0; i < s.Count; i++)
+        for (var i = 1; i < s.Count; i++)
         {
-            var item = s[i];
-            if (item != key) continue;
-            if (i - 1 < 0)
-                continue;
+            var curr = s[i];
+            if (curr != key) continue;
 
-            var item2 = s[i - 1];
-            return item2 switch
+            var prev = s[i - 1];
+            return prev switch
             {
                 "PRIMA" => 1,
                 "SECONDA" => 2,
@@ -78,12 +74,12 @@ public class RankingOrder
     public string GetId()
     {
         var idList = new List<string>();
-        if (Anticipata == true) idList.Add($"anticipata");
+        if (IsAnticipata) idList.Add($"anticipata");
         if (Primary != null) idList.Add($"{Primary}fase");
         if (Secondary != null) idList.Add($"{Secondary}grad");
         
         var cleanPhase = Phase?.Replace("_", "").Replace("-", "").Replace(" ", "_").ToLower() ?? "";
-        var noOrder = Anticipata == false && Primary == null && Secondary == null; 
+        var noOrder = IsAnticipata == false && Primary == null && Secondary == null; 
         var isSingleExtraEu = noOrder && cleanPhase.Contains("extraue");
 
         if (noOrder)
@@ -92,7 +88,7 @@ public class RankingOrder
         }
         
         idList.Add(IsEnglish ? "eng" : "ita");
-        if (ExtraEu == true && !isSingleExtraEu) idList.Add("extraeu"); // the second condition is to avoid double extraeu
+        if (IsExtraEu && !isSingleExtraEu) idList.Add("extraeu"); // the second condition is to avoid double extraeu
         
         var id = string.Join("_", idList);
         return id;
@@ -104,17 +100,15 @@ public class RankingOrder
         i ^= Phase?.GetHashCode() ?? "Phase".GetHashCode();
         i ^= Primary?.GetHashCode() ?? "Primary".GetHashCode();
         i ^= Secondary?.GetHashCode() ?? "Secondary".GetHashCode();
-        i ^= ExtraEu?.GetHashCode() ?? "ExtraEu".GetHashCode();
+        i ^= IsExtraEu.GetHashCode();
 
         return i;
     }
 
     public void Merge(RankingOrder? rankingRankingOrder)
     {
-        Anticipata ??= rankingRankingOrder?.Anticipata;
         Phase ??= rankingRankingOrder?.Phase;
         Primary ??= rankingRankingOrder?.Primary;
         Secondary ??= rankingRankingOrder?.Secondary;
-        ExtraEu ??= rankingRankingOrder?.ExtraEu;
     }
 }
